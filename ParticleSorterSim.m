d8a63389e31e14779a39ed2550ed3ce51b212b7c
Ryan Marks <ryan@sigmabot.com>
2014-03-22 19:57:07 -0400

    Added more modular B fields, drawing B fields

--- ParticleSorterSim.m
+++ ParticleSorterSim.m
@@ -1,11 +1,11 @@
+function [] = ParticleSorterSim ()
 %      y
 %      |
-%      O--x
+%      Z--x
 %     /
-%    Z
 %[x y z]
-simTime=1*10^-8;%also seconds
-steps=1000
+simTime=2*10^-8;%also seconds
+steps=500;
 deltaT=simTime/steps;%seconds
 
 t=0;
@@ -13,12 +13,13 @@ t=0;
 
 %B field is uniform
 c=2.99*10^8;
-b=[0,0,0*10^-3];%units are in teslas
-%Electric field
-e=[0,1,0];
-magLength=0.56;
+bAMagnitude=[0,0,1];%units are in teslas
+bAArea=[1,-0.17,-0.5;1.54,0.17,.5;];
 
-pType=1;
+bBMagnitude=[0,0,-1];%units are in teslas
+bBArea=[3,-0.5,-0.5;3.54,-0.2,.5;];
+
+pType=2;
 %1 Electron
 %2 Proton
 %3 Pion
@@ -39,18 +40,49 @@ switch pType
     
 end
 
-sParticle=[0,0.5,0];
+sParticle=[0,0,0];
 hold on;
+axis equal;
+drawBField(bAArea,bAMagnitude);
+drawBField(bBArea,bBMagnitude);
 
 while(t<simTime)
-    if sParticle(1)<magLength
-        a=chargeMassRatio*cross(vParticle,b)+chargeMassRatio*e;
+    
+    if inside(sParticle,bAArea)
+        a=chargeMassRatio*cross(vParticle,bAMagnitude);
+    elseif inside(sParticle,bBArea)
+        a=chargeMassRatio*cross(vParticle,bBMagnitude);
     else
-        a=[0,0,0];
+        a = [0,0,0];
     end
     sParticle=sParticle+vParticle*deltaT+(1/2)*a*deltaT*deltaT;
     vParticle=vParticle+a*deltaT;
     t=t+deltaT;
     plot3(sParticle(1),sParticle(2),sParticle(3),drawAs);
     drawnow;
+end
+end 
+
+function [isInside] = inside(point,boundingArea)
+    isInside=true;
+    for i=1:3
+        if point(i)>boundingArea(1,i)&&point(i)>boundingArea(2,i)
+            isInside=false;
+        end   
+        if point(i)<boundingArea(1,i)&&point(i)<boundingArea(2,i)
+            isInside=false;
+        end  
+    end
+end
+
+function [] = drawBField(bArea,bMagnitude)
+    arrowsPerAxis=2;
+    bMagnitude=bMagnitude*0.5/norm(bMagnitude);
+    for x=min(bArea(:,1)):(max(bArea(:,1))-min(bArea(:,1)))/arrowsPerAxis:max(bArea(:,1))
+        for y=min(bArea(:,2)):(max(bArea(:,2))-min(bArea(:,2)))/arrowsPerAxis:max(bArea(:,2))
+            for z=min(bArea(:,3)):(max(bArea(:,3))-min(bArea(:,3)))/arrowsPerAxis:max(bArea(:,3))
+                quiver3(x,y,z,bMagnitude(1),bMagnitude(2),bMagnitude(3),'black');
+            end
+        end
+    end
 end
